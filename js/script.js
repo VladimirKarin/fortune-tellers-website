171ff3fcd564a408e6db9f1394e7abb80393aa63
Vladimir Karin <Vladimir.Karin@gmail.com>
2024-08-04 16:14:19 +0300

    Navigation menu, responsive + mobile

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@ const toggleMobileNav = () => {
 
 const navigationButtonElement = document.querySelector('.btn-mobile-nav');
 navigationButtonElement.addEventListener('click', toggleMobileNav);
+
+document
+    .querySelector('.btn-mobile-nav')
+    .addEventListener('click', function () {
+        const nav = document.querySelector('.main-nav');
+        const openIcon = document.querySelector(
+            ".icon-mobile-nav[name='color-wand']"
+        );
+        const closeIcon = document.querySelector(
+            ".icon-mobile-nav[name='close']"
+        );
+
+        if (nav.style.display === 'block') {
+            nav.style.display = 'none';
+            openIcon.style.display = 'block';
+            closeIcon.style.display = 'none';
+        } else {
+            nav.style.display = 'block';
+            openIcon.style.display = 'none';
+            closeIcon.style.display = 'block';
+        }
+    });
