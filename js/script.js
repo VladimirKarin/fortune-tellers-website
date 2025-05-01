364194e8106e11741b5d1ad649d497a5d0554612
Vladimir Karin <Vladimir.Karin@gmail.com>
2025-05-01 13:50:36 +0300

    Countdowns Section: finished styling and JS logic for the timer component

--- js/script.js
+++ js/script.js
@@ -273,3 +273,39 @@ document.addEventListener('DOMContentLoaded', function () {
     const popupManager = new PopupManager();
     const carousel = new Carousel();
 });
+
+// ------------------------------------------------------------------
+// Countdown clock
+// ------------------------------------------------------------------
+
+const Days = document.getElementById('days');
+const Hours = document.getElementById('hours');
+const Minutes = document.getElementById('minutes');
+const Seconds = document.getElementById('seconds');
+
+const targetDate = new Date('May 31 2025 00:00:00').getTime();
+console.log(targetDate);
+
+function timer() {
+    const currentDate = new Date().getTime();
+    const timeDifference = targetDate - currentDate; //miliseconds
+
+    const days = Math.floor(timeDifference / 1000 / 60 / 60 / 24);
+    const hours = Math.floor(timeDifference / 1000 / 60 / 60) % 24;
+    const minutes = Math.floor(timeDifference / 1000 / 60) % 60;
+    const seconds = Math.floor(timeDifference / 1000) % 60;
+
+    Days.innerHTML = days;
+    Hours.innerHTML = hours;
+    Minutes.innerHTML = minutes;
+    Seconds.innerHTML = seconds;
+
+    if (timeDifference < 0) {
+        Days.innerHTML = '00';
+        Hours.innerHTML = '00';
+        Minutes.innerHTML = '00';
+        Seconds.innerHTML = '00';
+    }
+}
+
+setInterval(timer, 1000);
